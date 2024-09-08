ebd9a129a918c3b644a390d60f085aae7a07df65
Bruno Perez <konobabuno@gmail.com>
2024-09-08 13:13:19 -0600

    remove env

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,34 +2,22 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
-// Simula una base de datos con contraseñas encriptadas
-const usuarios = [
-    { 
-      id: 1, 
-      nombre: 'Mediterrum', 
-      email: 'hey@mediterrum.com', 
-      password: bcrypt.hashSync(process.env.JWT_PASSWORD, 10)  // Encripta la contraseña "123456"
-    }
-  ];
+// Contraseña encriptada almacenada en .env
+const storedPassword = bcrypt.hashSync(process.env.JWT_PASSWORD, 10);
 
-// Controlador para el login
+// Controlador para el login basado solo en la contraseña
 const login = (req, res) => {
-  const { email, password } = req.body;
-  const usuario = usuarios.find(u => u.email === email);
+  const { password } = req.body;
 
-  if (usuario) {
-    // Compara la contraseña enviada con la guardada encriptada
-    const isMatch = bcrypt.compareSync(password, usuario.password);
+  // Compara la contraseña enviada con la guardada encriptada
+  const isMatch = bcrypt.compareSync(password, storedPassword);
 
-    if (isMatch) {
-      // Generar un token JWT
-      const token = jwt.sign({ id: usuario.id, email: usuario.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
-      return res.json({ token });
-    } else {
-      return res.status(400).json({ message: 'Contraseña incorrecta' });
-    }
+  if (isMatch) {
+    // Generar un token JWT solo usando la contraseña y el JWT_SECRET
+    const token = jwt.sign({ password: process.env.JWT_PASSWORD }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    return res.json({ token });
   } else {
-    return res.status(400).json({ message: 'Usuario no encontrado' });
+    return res.status(400).json({ message: 'Contraseña incorrecta' });
   }
 };
 
